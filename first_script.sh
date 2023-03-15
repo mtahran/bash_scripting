8159a6f7256d349f052b504701967cc9a25b07fb
mtahran <mutahran@gmail.com>
2023-03-15 01:50:43 +0000

    MORE

--- first_script.sh
+++ first_script.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+
+dir=$(find / -type d -name $1)
+
 echo "Hello World" ## $0 represents script itself by presenting the name
 echo "$1 $2 $3 $4" ## any shell script by default has 9 arguments
 echo "these are the all arguments $@" ## $@ represents all arguments
 echo "this is the PID: $$"
-## git pull
-## git add .
-## git commit -m "comments"
-## git push 
+# git pull
+# git add .
+# git commit -m "comments"
+# git push 
+
+
+
+
+
+
 
 
