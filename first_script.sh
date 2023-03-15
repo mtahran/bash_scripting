415c60fdd78d263c5276fd23db7b6646850e4374
mtahran <mutahran@gmail.com>
2023-03-15 00:52:53 +0000

    basic scripting samples

---
+++ first_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello World" ## $0 represents script itself by presenting the name
+echo "$1 $2 $3 $4" ## any shell script by default has 9 arguments
+echo "these are the all arguments $@" ## $@ represents all arguments
+echo "this is the PID: $$"
