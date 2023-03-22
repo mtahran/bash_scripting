e83978f2326737bc1a449318bfd2d28c432043ac
mtahran <mutahran@gmail.com>
2023-03-22 01:29:12 +0000

    more updates on practices

--- shell/for_loop.sh
+++ shell/for_loop.sh
@@ -34,9 +34,9 @@
 #     command-3
 # done
 
-x=1
-while [ $x -le 5 ]; do
-    echo "Welcome $x times"
-     x=$(($x +1))
-done
+# x=1
+# while [ $x -le 5 ]; do
+#     echo "Welcome $x times"
+#      x=$(($x +1))
+# done
 
